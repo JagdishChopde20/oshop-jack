fdf3b6e3faa6cd1b90d6e051a43309e694e923ce
Jagdish Chopde <jagdishchopde@outlook.com>
2019-08-25 23:15:03 +0530

    committed missing file in section Product Catalouge.

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -4,7 +4,7 @@ import { Product } from './product';
 export class ShoppingCart {
   items: ShoppingCartItem[] = [];
 
-  constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+  constructor(private itemsMap: { [productId: string]: ShoppingCartItem }) {
     this.itemsMap = itemsMap || {};
 
     for (let productId in itemsMap) {
